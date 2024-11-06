fec04b0c313abe6df16b312b1760ca375a67a704
Madboy0700 <bobimobi07@gmail.com>
2024-11-06 07:57:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,19 +15,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", 28121536 ))
+API_HASH = getenv("API_HASH","57d552d05f2a76244291d9eb330294c2")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN","7272255274:AAHix6fyTVhU54oO0kJ3kQJyM-TivDVyi4k")
 
 # SUDO USERS
 SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
+    map(int, getenv("SUDO_USER", "@AcelyaAsissistan").split())
 )  # Input type must be interger
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1002461218502"))
 
 # Message to display when someone starts your bot
 PRIVATE_START_MESSAGE = getenv(
